89484b5b867b0ed515fd390ceb6cbd30d6c0e67f
Dillon Fagan <dillonfagan@pm.me>
2020-08-19 21:31:15 -0400

    Add title to page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Starboard Developer Cooperative</title>
+      </Head>
       <h1 className="text-blue-500 text-6xl">Starboard Co-op</h1>
       <h2 className="text-xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h2>
     </main>
