d2d42455e66b96dd4da6a0030e577e427e4e2f3c
Dillon Fagan <dillonfagan@pm.me>
2020-08-19 22:25:48 -0400

    Iterate on layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-1/2 m-auto">
       <Head>
         <title>Starboard Developer Cooperative</title>
       </Head>
-      <h1 className="text-blue-500 text-6xl">Starboard Co-op</h1>
-      <h2 className="text-xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h2>
+      <div className="flex">
+        <h1 className="text-blue-500 text-6xl">Starboard Co-op</h1>
+        <h2 className="text-blue-700 text-xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h2>
+      </div>
     </main>
   )
 }
