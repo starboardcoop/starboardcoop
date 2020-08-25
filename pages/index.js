805af11560a8796bb9adf99f7a14e1bb0b038d16
Dillon Fagan <dillonfagan@pm.me>
2020-08-24 20:55:52 -0400

    Update styling and add logo

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <main className="w-1/2 m-auto">
-      <Head>
-        <title>Starboard Developer Cooperative</title>
-      </Head>
-      <div className="flex">
-        <h1 className="text-blue-500 text-6xl">Starboard Co-op</h1>
-        <h2 className="text-blue-700 text-xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h2>
-      </div>
-    </main>
-  )
-}
+const Home = () => (
+  <main className="sm:max-w-full md:w-1/2 lg:1/3 sm:mx-3 md:mx-auto lg:mx-auto">
+    <Head>
+      <title>Starboard Developer Cooperative</title>
+    </Head>
+    <div className="flex flex-col">
+      <img className="w-12 rounded-full" src="/logo.png" alt="Starboard" />
+      <h1 className="text-gray-dark text-6xl">Starboard Co-op</h1>
+      <h2 className="text-gray-light text-xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h2>
+      <p className="mt-4">
+        <a className="text-primary" href="https://riceo.org/learn">Learn</a> about the worker cooperative movement in Rhode Island.
+      </p>
+    </div>
+  </main>
+)
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    colors: {
+      white: '#ffffff',
+      primary: '#03989e',
+      gray: {
+        light: '#a6a6a6',
+        dark: '#545454'
+      }
+    }
   },
   variants: {},
   plugins: [],
