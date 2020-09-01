f8e7db4085e2d9cdb4ebf76525ffe5ca0a0bc021
Chris Milkaitis <chris.milkaitis@gmail.com>
2020-08-31 21:22:52 -0400

    Created navbar and updated logo for mobile

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,21 @@
 import Link from 'next/link'
 
+const headerStyle = "w-full bg-primary"
+const navStyle = "flex bg-gray-dark text-white text-xl p-4"
+
 const Header = () => (
-    <header>
-        <div>Logo</div>
-        <div>
-            <Link href="/"><a>NavOne</a></Link>
-            <Link href="/"><a>NavTwo</a></Link>
-            <Link href="/"><a>NavThree</a></Link>
+    <header className={headerStyle}>
+        <div className="sm:w-full md:w-40">
+            <img src="/masthead-white.png" alt="Starboard" />
         </div>
-        <div>socials</div>
+        <nav className={navStyle}>
+            <div>
+                <Link href="/"><a className="mr-4">NavOne</a></Link>
+                <Link href="/"><a className="mr-4">NavTwo</a></Link>
+                <Link href="/"><a className="mr-4">NavThree</a></Link>
+            </div>
+            <div>socials</div>
+        </nav>
     </header>
 )
 
