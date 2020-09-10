ff2d1fd3c155e951f80c4e65887d4a2cfe52d92b
mdantuono <mikeishere3@gmail.com>
2020-09-09 22:22:54 -0400

    Refactored all Sections to use className

--- components/Section.js
+++ components/Section.js
@@ -1,5 +1,5 @@
 const Section = props => (
-    <section className={`py-20 px-8 text-center bg-top bg-cover bg-${props.backgroundImage} bg-${props.color} text-${props.textColor} ${props.className}`}>
+    <section className={`py-20 px-8 text-center bg-top bg-cover ${props.className}`}>
         <div className="md:max-w-md my-0 mx-auto">{props.children}</div>
     </section>
 )
--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,19 @@ const Home = () => (
     </Head>
     <Header />
     <main>
-      <Section className="md:h-70" backgroundImage="cashier" textColor="white">
+      <Section className="md:h-70 text-white bg-cashier">
         <h1 className="text-white text-3xl md:text-4xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h1>
         <p className="mt-4 text-xl">
           <a className="text-primary-light" href="https://riceo.org/learn">Learn</a> about the worker cooperative movement in Rhode Island.
         </p>
       </Section>
-      <Section color="gray-dark" textColor="white">
+      <Section className="bg-gray-dark text-white">
         <h2 className="text-2xl">We bring Rhode Island small businesses online so they can better serve their customers and gain new ones.</h2>
       </Section>  
-      <Section color="primary-dark" textColor="white">
+      <Section className="bg-primary-dark text-white">
         <h2 className="text-2xl">The global Covid-19 pandemic has uncovered the need for small businesses to offer great online experiences, if they are to compete with large corporations.</h2>
       </Section>
-      <Section color="gray-dark" textColor="white">
+      <Section className="bg-gray-dark text-white">
         <h2 className="text-2xl">We believe a small, local store with a great website has a fighting chance against Walmart and Amazon.</h2>
       </Section>
     </main>
