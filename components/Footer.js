ffcc465db5d84c653d38e0fb41e31f5bc8840181
mdantuono <mikeishere3@gmail.com>
2020-09-14 20:38:10 -0400

    Add love to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
     <footer className="bg-gray-dark text-white flex flex-col justify-center text-center text-xs h-8">
-        <p>This site was built by StarboardCoOp in Rhode Island.</p>
+        <p>Made with ❤️ in Rhode Island.</p>
     </footer>
 )
 
