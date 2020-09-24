f19bfe162b98a72113056803d37383cdf194cdca
Dillon Fagan <dillonfagan@pm.me>
2020-09-23 22:46:31 -0400

    Update section styling

--- pages/index.js
+++ pages/index.js
@@ -11,24 +11,25 @@ const Home = () => (
     <Header />
     <main>
       <Section id="one" className="md:h-70 text-white bg-cashier flex flex-col justify-center">
-        <h1 className="text-white text-3xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h1>
+        <h1 className="text-white text-3xl my-16">Your friendly neighborhood software development studio, owned by the people who do the work.</h1>
         <p className="mt-4 text-xl">
-          Learn about the <a className="text-white bg-primary-light p-1 hover:bg-primary-dark" href="https://riceo.org/learn">worker cooperative movement</a> in Rhode Island.
+          Learn about the <a className="text-white bg-primary-light p-1 hover:bg-primary-dark" href="https://riceo.org/learn">worker cooperative</a> movement in Rhode Island.
         </p>
       </Section>
       <Section id="two" className="bg-gray-dark text-white">
-        <h2 className="text-2xl">We bring Rhode Island small businesses online so they can better serve their customers and gain new ones.</h2>
-      </Section>  
-      <Section id="three" className="bg-primary-dark text-white">
-        <h2 className="text-2xl">The global Covid-19 pandemic has uncovered the need for small businesses to offer great online experiences, if they are to compete with large corporations.</h2>
+        <h2 className="text-2xl">We bring Rhode Island's small businesses online.</h2>
       </Section>
-      <Section id="four" className="md:h-70 text-white bg-brewery flex flex-col justify-center">
-        <h2 className="text-3xl md:text-4xl">We believe a small, local business with a great website has a fighting chance against the Walmarts and Amazons.</h2>
+      <Section id="three" className="h-70 text-white bg-brewery flex flex-col justify-center">
+        <h2 className="text-3xl md:text-4xl">We believe in small local businesses.</h2>
+        <p className="mt-4 text-xl">A great website has a fighting chance against the Walmarts and Amazons.</p>
       </Section>
-      <Section id="five" className="bg-gray-light text-gray-dark">
+      <Section id="four" className="bg-primary-dark text-white">
+        <h2 className="text-2xl">The global Covid-19 pandemic has uncovered the need for small businesses to offer great online experiences.</h2>
+      </Section>
+      <div id="five" className="bg-gray-light text-gray-dark text-center py-8">
         <h2>Contact Us!</h2>
         <a href = "mailto: starboardcoop@gmail.com">at Hello@Starboard.dev</a>
-      </Section>
+      </div>
     </main>
     <Footer />
   </>
