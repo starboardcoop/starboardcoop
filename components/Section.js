dbdc781fc5c3e3b52074781237128a00b711f2d1
Dillon Fagan <dillonfagan@pm.me>
2020-09-23 21:45:15 -0400

    Update heading font

--- components/Section.js
+++ components/Section.js
@@ -1,6 +1,6 @@
 const Section = props => (
     <section className={`py-20 px-8 text-center bg-top bg-cover ${props.className}`}>
-        <div id={props.id} className="md:max-w-md my-0 mx-auto">{props.children}</div>
+        <div id={props.id} className="md:max-w-lg my-0 mx-auto">{props.children}</div>
     </section>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Home = () => (
     <Header />
     <main>
       <Section id="one" className="md:h-70 text-white bg-cashier flex flex-col justify-center">
-        <h1 className="text-white text-3xl md:text-4xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h1>
+        <h1 className="text-white text-3xl">Your friendly neighborhood software development studio, owned by the people who do the work.</h1>
         <p className="mt-4 text-xl">
           Learn about the <a className="text-white bg-primary-light p-1 hover:bg-primary-dark" href="https://riceo.org/learn">worker cooperative movement</a> in Rhode Island.
         </p>
