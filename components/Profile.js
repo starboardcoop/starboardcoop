49735e8a8bb477f7cdd4527663da7eecb2ee3720
Dillon Fagan <dillonfagan@pm.me>
2020-10-07 19:06:02 -0400

    Adjust colors

--- components/Profile.js
+++ components/Profile.js
@@ -1,6 +1,6 @@
 const Profile = ({ name, title, image }) => (
-    <div className="bg-red-200 mb-8 md:mb-0">
-        <img className="h-56 w-56 mx-auto border-8 border-primary-dark rounded-full" src={image} alt={name} />
+    <div className="mb-8 md:mb-0">
+        <img className="h-56 w-56 mx-auto border-8 border-white rounded-full" src={image} alt={name} />
         <div className="text-center mt-4">
             <h3 className="font-bold text-xl">{name}</h3>
             <h4 className="text-base">{title}</h4>
